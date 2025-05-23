fba528db98068d5e96b13cf491dfa0513ba6ce41
sunfishes <cowjung1024@naver.com>
2025-05-23 16:27:38 +0900

    add: edit_screen container

--- lib/screen/edit_screen.dart
+++ lib/screen/edit_screen.dart
@@ -45,18 +45,100 @@ final TextEditingController _feedbackController = TextEditingController();
             crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('제목'),
-            TextField(
-              style: TextStyle(fontSize: 12, color: Colors.black),
-              controller: _titleController,
+            Container(
+              margin: EdgeInsets.only(top: 8),
+
+              height: 41,
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xffd9d9d9),
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+
+                  ),
+                  hintText: '아이디어 제목',
+                  hintStyle: TextStyle(color: Color(0xffb4b4b4),)
+                ),
+                style: TextStyle(fontSize: 12, color: Colors.black),
+                controller: _titleController,
+              ),
             ),
+            SizedBox(height: 16,),
             Text('아이디어를 작성한 계기'),
+            Container(
+              margin: EdgeInsets.only(top: 8),
 
+              height: 41,
+              child: TextField(
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xffd9d9d9),
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+
+                    ),
+                    hintText: '아이디어를 떠올린 계기',
+                    hintStyle: TextStyle(color: Color(0xffb4b4b4),)
+                ),
+                style: TextStyle(fontSize: 12, color: Colors.black),
+                controller: _motiveController,
+              ),
+            ),
+            SizedBox(height: 16,),
             Text('아이디어 내용'),
+            Container(
+              margin: EdgeInsets.only(top: 8),
+              child: TextField(
+                maxLength: 500,
+                maxLines: 5,
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xffd9d9d9),
+                      ),
+                      borderRadius: BorderRadius.circular(10),
 
+                    ),
+                    hintText: '떠오르신 아이디어를 자세하게 작성해주세요',
+                    hintStyle: TextStyle(color: Color(0xffb4b4b4),)
+                ),
+                style: TextStyle(fontSize: 12, color: Colors.black),
+                controller: _contentController,
+              ),
+            ),
+            SizedBox(height: 16,),
             Text('아이디어 중요도 점수'),
 
+            SizedBox(height: 16,),
             Text('유저 피드백 사항(선택)'),
+            Container(
+              margin: EdgeInsets.only(top: 8),
+              child: TextField(
+                maxLength: 500,
+                maxLines: 5,
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xffd9d9d9),
+                      ),
+                      borderRadius: BorderRadius.circular(10),
 
+                    ),
+                    hintText: '떠오르신 아이디어 기반으로\n전달받은 피드백들을 정리해주세요',
+                    hintStyle: TextStyle(color: Color(0xffb4b4b4),)
+                ),
+                style: TextStyle(fontSize: 12, color: Colors.black),
+                controller: _feedbackController,
+              ),
+            ),
+            SizedBox(height: 16,),
                 ],
                 ),
         ),
